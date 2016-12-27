6f327ee05bae51fdd29789ba6e85ca7a6f333ade
Da-Technomancer <mizrahi.max@comcast.net>
2016-12-27 13:06:27 -0500

    Fixed world corruption with time
    Fixed a world breaking infinite loops when using time on a reflector.

--- src/main/java/com/Da_Technomancer/crossroads/API/effects/TimeEffect.java
+++ src/main/java/com/Da_Technomancer/crossroads/API/effects/TimeEffect.java
@@ -2,8 +2,11 @@ package com.Da_Technomancer.crossroads.API.effects;
 
 import java.util.Random;
 
+import com.Da_Technomancer.crossroads.API.Capabilities;
+
 import net.minecraft.init.Blocks;
 import net.minecraft.tileentity.TileEntitySign;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ITickable;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.ITextComponent;
@@ -12,10 +15,16 @@ import net.minecraft.world.World;
 public class TimeEffect implements IEffect{
 
 	private final Random rand = new Random();
-	
+
 	@Override
 	public void doEffect(World worldIn, BlockPos pos, double mult){
 		if(worldIn.getTileEntity(pos) instanceof ITickable){
+			for(EnumFacing dir : EnumFacing.values()){
+				if(worldIn.getTileEntity(pos).hasCapability(Capabilities.MAGIC_HANDLER_CAPABILITY, dir)){
+					return;
+				}
+			}
+
 			for(int i = rand.nextInt((int) mult); i < mult; i++){
 				((ITickable) worldIn.getTileEntity(pos)).update();
 			}
