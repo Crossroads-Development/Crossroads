af460c3bfa491a01579b12b11191ab63ea7cc499
Da-Technomancer <mizrahi.max@comcast.net>
2017-04-29 10:07:24 -0400

    Overlay for magic items

--- src/main/java/com/Da_Technomancer/crossroads/EventHandlerClient.java
+++ src/main/java/com/Da_Technomancer/crossroads/EventHandlerClient.java
@@ -10,6 +10,7 @@ import com.Da_Technomancer.crossroads.API.enums.GoggleLenses;
 import com.Da_Technomancer.crossroads.API.packets.SafeCallable;
 import com.Da_Technomancer.crossroads.API.technomancy.FieldWorldSavedData;
 import com.Da_Technomancer.crossroads.API.technomancy.LooseBeamRenderable;
+import com.Da_Technomancer.crossroads.items.MagicUsingItem;
 import com.Da_Technomancer.crossroads.items.ModItems;
 
 import net.minecraft.client.Minecraft;
@@ -23,14 +24,19 @@ import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.chunk.Chunk;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public final class EventHandlerClient{
 
-	private static final ResourceLocation TEXTURE_FIELDS = new ResourceLocation(Main.MODID, "textures/model/field.png");
+	private static final ResourceLocation TEXTURE_FIELDS = new ResourceLocation(Main.MODID, "textures/gui/field.png");
 
 	@SubscribeEvent
 	public void drawFieldsAndBeams(RenderWorldLastEvent e){
@@ -117,7 +123,7 @@ public final class EventHandlerClient{
 			Minecraft.getMinecraft().getTextureManager().bindTexture(TileEntityBeaconRenderer.TEXTURE_BEACON_BEAM);
 
 			ArrayList<LooseBeamRenderable> toRemove = new ArrayList<LooseBeamRenderable>();
-			
+
 			for(LooseBeamRenderable beam : SafeCallable.beamsToRender){
 				GlStateManager.pushMatrix();
 				GlStateManager.pushAttrib();
@@ -157,7 +163,7 @@ public final class EventHandlerClient{
 				GlStateManager.color(1, 1, 1);
 				GlStateManager.popAttrib();
 				GlStateManager.popMatrix();
-				
+
 				if(beam.lastTick != game.world.getTotalWorldTime()){
 					beam.lastTick = game.world.getTotalWorldTime();
 					if(beam.lifeTime-- < 0){
@@ -165,7 +171,7 @@ public final class EventHandlerClient{
 					}
 				}
 			}
-			
+
 			for(LooseBeamRenderable beam : toRemove){
 				SafeCallable.beamsToRender.remove(beam);
 			}
@@ -196,6 +202,99 @@ public final class EventHandlerClient{
 						ent.setGlowing(true);
 					}
 				}
+			}
+		}
+	}
+
+	private static final ResourceLocation MAGIC_BAR_BACKGROUND = new ResourceLocation(Main.MODID, "textures/gui/magic_info_back.png");
+	private static final ResourceLocation MAGIC_BAR_FOREGROUND = new ResourceLocation(Main.MODID, "textures/gui/magic_info_front.png");
+	private static final ResourceLocation COLOR_SHEET = new ResourceLocation(Main.MODID, "textures/blocks/color_sheet.png");
+	
+	@SubscribeEvent
+	public void magicUsingItemOverlay(RenderGameOverlayEvent e){
+		if(e.getType() == ElementType.HOTBAR){
+			EntityPlayer player = Minecraft.getMinecraft().player;
+			ItemStack offStack = player.getHeldItem(EnumHand.OFF_HAND);
+			if(offStack.getItem() == ModItems.beamCage && offStack.hasTagCompound()){
+				NBTTagCompound nbt = offStack.getTagCompound();
+				GlStateManager.pushMatrix();
+				GlStateManager.pushAttrib();
+				GlStateManager.enableBlend();
+				Minecraft.getMinecraft().getTextureManager().bindTexture(MAGIC_BAR_BACKGROUND);
+				Tessellator tes = Tessellator.getInstance();
+				VertexBuffer buf = tes.getBuffer();
+				buf.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX);
+				buf.pos(0, 120, -3).tex(0, 1).endVertex();
+				buf.pos(117, 120, -3).tex(1, 1).endVertex();
+				buf.pos(117, 60, -3).tex(1, 0).endVertex();
+				buf.pos(0, 60, -3).tex(0, 0).endVertex();
+				tes.draw();
+				
+				Minecraft.getMinecraft().getTextureManager().bindTexture(COLOR_SHEET);
+				buf.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX_COLOR);
+				for(int i = 0; i < 4; i++){
+					int extension = 9 * nbt.getInteger(i == 0 ? "stored_ENERGY" : i == 1 ? "stored_POTENTIAL" : i == 2 ? "stored_STABILITY" : "stored_VOID") / 128;
+					buf.pos(24, 84 + (9 * i), -2).tex(.25F + (((float) i) * .0625F), .0625F).color(i == 0 ? 255 : 0, i == 1 ? 255 : 0, i == 2 ? 255 : 0, 255).endVertex();
+					buf.pos(24 + extension, 84 + (9 * i), -2).tex(.3125F + (((float) i) * .0625F), .0625F).color(i == 0 ? 255 : 0, i == 1 ? 255 : 0, i == 2 ? 255 : 0, 255).endVertex();
+					buf.pos(24 + extension, 78 + (9 * i), -2).tex(.3125F + (((float) i) * .0625F), 0).color(i == 0 ? 255 : 0, i == 1 ? 255 : 0, i == 2 ? 255 : 0, 255).endVertex();
+					buf.pos(24, 78 + (9 * i), -2).tex(.25F + (((float) i) * .0625F), 0).color(i == 0 ? 255 : 0, i == 1 ? 255 : 0, i == 2 ? 255 : 0, 255).endVertex();
+				}
+				tes.draw();
+				
+				Minecraft.getMinecraft().getTextureManager().bindTexture(MAGIC_BAR_FOREGROUND);
+				buf.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX);
+				buf.pos(0, 120, -1).tex(0, 1).endVertex();
+				buf.pos(117, 120, -1).tex(1, 1).endVertex();
+				buf.pos(117, 60, -1).tex(1, 0).endVertex();
+				buf.pos(0, 60, -1).tex(0, 0).endVertex();
+				tes.draw();
+				
+				Minecraft.getMinecraft().fontRendererObj.drawString(offStack.getDisplayName(), 16, 65, Color.DARK_GRAY.getRGB());
+				GlStateManager.color(1, 1, 1);
+				GlStateManager.disableBlend();
+				GlStateManager.popAttrib();
+				GlStateManager.popMatrix();
+			}
+			ItemStack mainStack = player.getHeldItem(EnumHand.MAIN_HAND);
+			if(mainStack.getItem() instanceof MagicUsingItem && mainStack.hasTagCompound()){
+				NBTTagCompound nbt = mainStack.getTagCompound();
+				GlStateManager.pushMatrix();
+				GlStateManager.pushAttrib();
+				GlStateManager.enableBlend();
+				Minecraft.getMinecraft().getTextureManager().bindTexture(MAGIC_BAR_BACKGROUND);
+				Tessellator tes = Tessellator.getInstance();
+				VertexBuffer buf = tes.getBuffer();
+				buf.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX);
+				buf.pos(0, 60, -3).tex(0, 1).endVertex();
+				buf.pos(117, 60, -3).tex(1, 1).endVertex();
+				buf.pos(117, 0, -3).tex(1, 0).endVertex();
+				buf.pos(0, 0, -3).tex(0, 0).endVertex();
+				tes.draw();
+				
+				Minecraft.getMinecraft().getTextureManager().bindTexture(COLOR_SHEET);
+				buf.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX_COLOR);
+				for(int i = 0; i < 4; i++){
+					int extension = 9 * nbt.getInteger(i == 0 ? "ENERGY" : i == 1 ? "POTENTIAL" : i == 2 ? "STABILITY" : "VOID");
+					buf.pos(24, 24 + (9 * i), -2).tex(.25F + (((float) i) * .0625F), .0625F).color(i == 0 ? 255 : 0, i == 1 ? 255 : 0, i == 2 ? 255 : 0, 255).endVertex();
+					buf.pos(24 + extension, 24 + (9 * i), -2).tex(.3125F + (((float) i) * .0625F), .0625F).color(i == 0 ? 255 : 0, i == 1 ? 255 : 0, i == 2 ? 255 : 0, 255).endVertex();
+					buf.pos(24 + extension, 18 + (9 * i), -2).tex(.3125F + (((float) i) * .0625F), 0).color(i == 0 ? 255 : 0, i == 1 ? 255 : 0, i == 2 ? 255 : 0, 255).endVertex();
+					buf.pos(24, 18 + (9 * i), -2).tex(.25F + (((float) i) * .0625F), 0).color(i == 0 ? 255 : 0, i == 1 ? 255 : 0, i == 2 ? 255 : 0, 255).endVertex();
+				}
+				tes.draw();
+				
+				Minecraft.getMinecraft().getTextureManager().bindTexture(MAGIC_BAR_FOREGROUND);
+				buf.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX);
+				buf.pos(0, 60, -1).tex(0, 1).endVertex();
+				buf.pos(117, 60, -1).tex(1, 1).endVertex();
+				buf.pos(117, 0, -1).tex(1, 0).endVertex();
+				buf.pos(0, 0, -1).tex(0, 0).endVertex();
+				tes.draw();
+				
+				Minecraft.getMinecraft().fontRendererObj.drawString(mainStack.getDisplayName(), 16, 5, Color.DARK_GRAY.getRGB());
+				
+				GlStateManager.disableBlend();
+				GlStateManager.popAttrib();
+				GlStateManager.popMatrix();
 			}
 		}
 	}
