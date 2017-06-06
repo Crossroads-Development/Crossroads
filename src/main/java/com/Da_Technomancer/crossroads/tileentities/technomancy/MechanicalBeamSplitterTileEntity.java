b59e57cdb6793f8a8d6921fab008492aa7618eea
Da-Technomancer <mizrahi.max@comcast.net>
2017-06-06 14:17:59 -0400

    Documented Mechanical Beam Splitter

--- src/main/java/com/Da_Technomancer/crossroads/tileentities/technomancy/MechanicalBeamSplitterTileEntity.java
+++ src/main/java/com/Da_Technomancer/crossroads/tileentities/technomancy/MechanicalBeamSplitterTileEntity.java
@@ -75,7 +75,7 @@ public class MechanicalBeamSplitterTileEntity extends BeamRenderTE implements IT
 			EnumFacing facing = world.getBlockState(pos).getValue(Properties.FACING);
 			TileEntity te = world.getTileEntity(pos.offset(facing));
 			double splitRatio = te != null && te.hasCapability(Capabilities.AXLE_HANDLER_CAPABILITY, facing.getOpposite()) ? Math.min(Math.abs(te.getCapability(Capabilities.AXLE_HANDLER_CAPABILITY, facing.getOpposite()).getMotionData()[0]), 15D) / 15D : 0;
-			MagicUnit outMult = out == null ? null : out.mult(splitRatio, false);
+			MagicUnit outMult = out == null ? null : new MagicUnit((int) Math.round(splitRatio * (double) out.getEnergy()), (int) Math.round(splitRatio * (double) out.getPotential()), (int) Math.round(splitRatio * (double) out.getStability()), (int) Math.round(splitRatio * (double) out.getVoid()));
 			if(outMult == null || outMult.getPower() == 0){
 				outMult = null;
 			}
