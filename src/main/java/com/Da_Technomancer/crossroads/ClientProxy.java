fbd3103a13c9fef6e16eda85ac70eb6facc94523
Da-Technomancer <mizrahi.max@comcast.net>
2017-06-10 20:52:28 -0400

    Fixed a crash on servers when compiled

--- src/main/java/com/Da_Technomancer/crossroads/ClientProxy.java
+++ src/main/java/com/Da_Technomancer/crossroads/ClientProxy.java
@@ -3,14 +3,19 @@ package com.Da_Technomancer.crossroads;
 import com.Da_Technomancer.crossroads.blocks.ModBlocks;
 import com.Da_Technomancer.crossroads.client.TESR.AAModTESR;
 import com.Da_Technomancer.crossroads.client.bakedModel.BakedModelLoader;
-import com.Da_Technomancer.crossroads.entity.ModEntities;
+import com.Da_Technomancer.crossroads.entity.EntityBullet;
 import com.Da_Technomancer.crossroads.fluids.ModFluids;
 import com.Da_Technomancer.crossroads.items.ModItems;
 import com.Da_Technomancer.crossroads.items.itemSets.GearFactory;
 import com.Da_Technomancer.crossroads.items.itemSets.HeatCableFactory;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.client.renderer.entity.RenderSnowball;
+import net.minecraft.init.Items;
 import net.minecraftforge.client.model.ModelLoaderRegistry;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -25,7 +30,7 @@ public class ClientProxy extends CommonProxy{
 		ModBlocks.preInitModels();
 		ModItems.initModels();
 		HeatCableFactory.clientInit();
-		ModEntities.clientInit();
+		RenderingRegistry.registerEntityRenderingHandler(EntityBullet.class, (RenderManager manager) -> (new RenderSnowball<EntityBullet>(manager, Items.field_191525_da, Minecraft.getMinecraft().getRenderItem())));
 	}
 
 	@Override
--- src/main/java/com/Da_Technomancer/crossroads/entity/ModEntities.java
+++ src/main/java/com/Da_Technomancer/crossroads/entity/ModEntities.java
@@ -2,15 +2,8 @@ package com.Da_Technomancer.crossroads.entity;
 
 import com.Da_Technomancer.crossroads.Main;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.entity.RenderManager;
-import net.minecraft.client.renderer.entity.RenderSnowball;
-import net.minecraft.init.Items;
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.registry.EntityRegistry;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
 public final class ModEntities{
 	
@@ -18,10 +11,5 @@ public final class ModEntities{
 		int id = 1;
 		EntityRegistry.registerModEntity(new ResourceLocation(Main.MODID, "bullet"), EntityBullet.class, "bullet", id++, Main.instance, 64, 5, true);
 		EntityRegistry.registerModEntity(new ResourceLocation(Main.MODID, "arm_ridable"), EntityArmRidable.class, "arm_ridable", id++, Main.instance, 64, 1, false);
-	}
-
-	@SideOnly(Side.CLIENT)
-	public static void clientInit(){
-		RenderingRegistry.registerEntityRenderingHandler(EntityBullet.class, (RenderManager manager) -> (new RenderSnowball<EntityBullet>(manager, Items.field_191525_da, Minecraft.getMinecraft().getRenderItem())));
 	}
 }
