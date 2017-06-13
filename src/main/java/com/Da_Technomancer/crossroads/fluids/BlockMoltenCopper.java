d2e5a9ba4216a925724b7e09654347c9af19af51
Da-Technomancer <mizrahi.max@comcast.net>
2017-06-12 20:56:43 -0400

    Bugfixes

--- src/main/java/com/Da_Technomancer/crossroads/fluids/BlockMoltenCopper.java
+++ src/main/java/com/Da_Technomancer/crossroads/fluids/BlockMoltenCopper.java
@@ -3,8 +3,11 @@ package com.Da_Technomancer.crossroads.fluids;
 import com.Da_Technomancer.crossroads.Main;
 
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraftforge.fluids.BlockFluidClassic;
 import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fluids.FluidRegistry;
@@ -21,6 +24,11 @@ public class BlockMoltenCopper extends BlockFluidClassic{
 		setRegistryName("blockMoltenCopper");
 		GameRegistry.register(this);
 		GameRegistry.register(new ItemBlock(this).setRegistryName("blockMoltenCopper"));
+	}
+
+	@Override
+	public int getLightValue(IBlockState state, IBlockAccess world, BlockPos pos){
+		return 15;
 	}
 
 	/**
--- src/main/java/com/Da_Technomancer/crossroads/fluids/BlockMoltenCopshowium.java
+++ src/main/java/com/Da_Technomancer/crossroads/fluids/BlockMoltenCopshowium.java
@@ -3,8 +3,11 @@ package com.Da_Technomancer.crossroads.fluids;
 import com.Da_Technomancer.crossroads.Main;
 
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraftforge.fluids.BlockFluidClassic;
 import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fluids.FluidRegistry;
@@ -22,6 +25,11 @@ public class BlockMoltenCopshowium extends BlockFluidClassic{
 		setRegistryName(name);
 		GameRegistry.register(this);
 		GameRegistry.register(new ItemBlock(this).setRegistryName(name));
+	}
+
+	@Override
+	public int getLightValue(IBlockState state, IBlockAccess world, BlockPos pos){
+		return 15;
 	}
 
 	/**
