4815d64a22c3e0ef54f5f50430ea046901a9acf2
Da-Technomancer <mizrahi.max@comcast.net>
2017-09-14 21:03:10 -0400

    More reactions

--- src/main/java/com/Da_Technomancer/crossroads/API/alchemy/AlchemyCraftingManager.java
+++ src/main/java/com/Da_Technomancer/crossroads/API/alchemy/AlchemyCraftingManager.java
@@ -41,7 +41,8 @@ public final class AlchemyCraftingManager{
 	private static final Color PHELOSTIGEN_COLOR = new Color(255, 200, 0, 150);
 	private static final Color CLEAR_COLOR = new Color(255, 255, 255, 0);
 	private static final Color TRANSLUCENT_WHITE_COLOR = new Color(255, 255, 255, 200);
-
+	private static final Color TRANSLUCENT_LIME_COLOR = new Color(200, 255, 0, 100);
+	
 	// Various effects
 	private static final AcidAlchemyEffect ACID_EFFECT = new AcidAlchemyEffect();
 
@@ -52,23 +53,26 @@ public final class AlchemyCraftingManager{
 		REAGENTS[1] = new SimpleReagentType("aether", -275D, -274D, 1, (MatterPhase phase) -> CLEAR_COLOR, null, 1, true, 0, null, null, 1, null);
 		REAGENTS[2] = new SimpleReagentType("adamant", Short.MAX_VALUE - 1, Short.MAX_VALUE, 2, (MatterPhase phase) -> Color.GRAY, null, 100, true, 0, null, SolventType.AQUA_REGIA, 0, null);// TODO item
 		REAGENTS[3] = new SimpleReagentType("sulfur", 115D, 445D, 3, (MatterPhase phase) -> phase == MatterPhase.GAS ? CLEAR_COLOR : phase == MatterPhase.LIQUID ? Color.RED : Color.YELLOW, null, 100, true, 0, null, SolventType.NON_POLAR, 0, null);// TODO item
-		REAGENTS[4] = new SimpleReagentType("water", 0D, 100D, 4, (MatterPhase phase) -> CLEAR_COLOR, Item.getItemFromBlock(Blocks.ICE), 1000, true, 0, SolventType.POLAR, null, 0, null);
+		REAGENTS[4] = new SimpleReagentType("water", 0D, 100D, 4, (MatterPhase phase) -> CLEAR_COLOR, Item.getItemFromBlock(Blocks.ICE), 100, true, 0, SolventType.POLAR, null, 0, null);
 		REAGENTS[5] = new SimpleReagentType("hydrogen_nitrate", -40D, 80D, 5, (MatterPhase phase) -> CLEAR_COLOR, null, 1, true, 0, null, SolventType.POLAR, 0, ACID_EFFECT);// Salt that forms nitric acid, AKA aqua fortis, in water.
 		REAGENTS[6] = new SimpleReagentType("sodium_chloride", 800D, 1400D, 6, (MatterPhase phase) -> phase == MatterPhase.LIQUID ? Color.ORANGE : Color.WHITE, ModItems.dustSalt, 100, true, 0, null, SolventType.POLAR, 0, new SaltEffect());// AKA table salt.
 		REAGENTS[7] = new SimpleReagentType("vanadium_5_oxide", 690D, 1750D, 7, (MatterPhase phase) -> Color.YELLOW, null, 100, true, 0, null, SolventType.POLAR, 0, null);// Vanadium (V) oxide. TODO item. This should decompose at the specified boiling point, but there isn't any real point to adding that.
 		REAGENTS[8] = new SimpleReagentType("sulfur_dioxide", -72D, -10D, 8, (MatterPhase phase) -> CLEAR_COLOR, null, 1, true, 0, SolventType.POLAR, SolventType.POLAR, 0, null);
 		REAGENTS[9] = new SimpleReagentType("sulfur_trioxide", 20D, 40D, 9, (MatterPhase phase) -> phase == MatterPhase.SOLID ? TRANSLUCENT_WHITE_COLOR : CLEAR_COLOR, null, 1, true, 0, null, SolventType.POLAR, 0, null);
 		REAGENTS[10] = new SimpleReagentType("hydrogen_sulfate", 10D, 340D, 10, (MatterPhase phase) -> CLEAR_COLOR, null, 1, true, 0, null, SolventType.POLAR, 0, ACID_EFFECT);// Salt that forms sulfuric acid, AKA Oil of Vitriol, in water.
-		REAGENTS[11] = new SimpleReagentType("aqua_regia", -40D, 100D, 11, (MatterPhase phase) -> Color.ORANGE, null, 1, true, 0, SolventType.AQUA_REGIA, null, 0, new AquaRegiaAlchemyEffect());// Shouldn't really be its own substance (actually a mixture of nitric and hydrochloric acid), but the code is greatly																																											// simplified by making it a separate substance.
+		REAGENTS[11] = new SimpleReagentType("aqua_regia", 0D, 100D, 11, (MatterPhase phase) -> Color.ORANGE, null, 1, true, 0, SolventType.AQUA_REGIA, null, 0, new AquaRegiaAlchemyEffect());// Shouldn't really be its own substance (actually a mixture of nitric and hydrochloric acid), but the code is greatly																																											// simplified by making it a separate substance.
 		REAGENTS[12] = new SimpleReagentType("murcury_2_sulfide", 580D, Short.MAX_VALUE, 12, (MatterPhase phase) -> Color.RED, Items.REDSTONE, 100, true, 0, null, null, 0, null);// Mercury (II) sulfide, AKA cinnabar.
 		REAGENTS[13] = new SimpleReagentType("murcury", -40D, 560D, 13, (MatterPhase phase) -> Color.LIGHT_GRAY, null, 1, true, 0, null, null, 0, null);// AKA quicksilver
 		REAGENTS[14] = new SimpleReagentType("gold", 1100D, 3000D, 14, (MatterPhase phase) -> Color.YELLOW, Items.GOLD_INGOT, 144, true, 0, null, SolventType.AQUA_REGIA, 0, null);
-		REAGENTS[15] = new SimpleReagentType("hydrogen_chloride", -110D, 90D, 15, (MatterPhase phase) -> CLEAR_COLOR, null, 1, true, 0, SolventType.POLAR, null, 0, ACID_EFFECT);// Salt that forms hydrochloric acid in water. Boiling point should be -90, set to 90 due to the alchemy system not allowing gasses to dissolve. 
-		REAGENTS[16] = new SimpleReagentType("sodium_sulfate", 890D, 1400D, 16, (MatterPhase phase) -> TRANSLUCENT_WHITE_COLOR, null, 1, true, 0, null, SolventType.POLAR, 0, null);
+		REAGENTS[15] = new SimpleReagentType("hydrogen_chloride", -110D, 90D, 15, (MatterPhase phase) -> CLEAR_COLOR, null, 1, true, 0, SolventType.POLAR, null, 0, ACID_EFFECT);// Salt that forms hydrochloric acid, AKA muriatic acid, in water. Boiling point should be -90, set to 90 due to the alchemy system not allowing gasses to dissolve. 
+		REAGENTS[16] = new SimpleReagentType("waste_salt", 900D, 1400D, 16, (MatterPhase phase) -> TRANSLUCENT_WHITE_COLOR, null, 1, true, 0, null, SolventType.POLAR, 0, null);//Any salt byproduct that is too boring to bother adding. TODO salt effect, item.  
 		REAGENTS[17] = new SimpleReagentType("ethanol", -110D, 80D, 17, (MatterPhase phase) -> CLEAR_COLOR, null, 1, true, 0, SolventType.NON_POLAR, null, 0, null);// If anyone asks, this is denatured alcohol for legal reasons.
-		REAGENTS[18] = new SimpleReagentType("philosopher_stone", -275D, -274D, 18, (MatterPhase phase) -> Color.BLACK, null, 100, true, 1, null, null, 2, null);// TODO item
-		REAGENTS[19] = new SimpleReagentType("practitioner_stone", -275D, -274D, 19, (MatterPhase phase) -> Color.BLACK, null, 100, true, 2, null, null, 2, null);// TODO item
-
+		REAGENTS[18] = new SimpleReagentType("philosopher_stone", Short.MAX_VALUE - 1, Short.MAX_VALUE, 18, (MatterPhase phase) -> Color.BLACK, null, 100, true, 1, null, null, 2, null);// TODO item
+		REAGENTS[19] = new SimpleReagentType("practitioner_stone", Short.MAX_VALUE - 1, Short.MAX_VALUE, 19, (MatterPhase phase) -> Color.BLACK, null, 100, true, 2, null, null, 2, null);// TODO item
+		REAGENTS[20] = new SimpleReagentType("bedrock", Short.MAX_VALUE - 1, Short.MAX_VALUE, 20, (MatterPhase phase) -> Color.GRAY, Item.getItemFromBlock(Blocks.BEDROCK), 100, true, 0, null, SolventType.AQUA_REGIA, 0, null);
+		REAGENTS[21] = new SimpleReagentType("chlorine", -100D, -35D, 21, (MatterPhase phase) -> TRANSLUCENT_LIME_COLOR, null, 1, true, 0, SolventType.NON_POLAR, SolventType.NON_POLAR, 0, null);//TODO add effect
+		REAGENTS[22] = new SimpleReagentType("alchemical_crystal", Short.MAX_VALUE - 1, Short.MAX_VALUE, 22, (MatterPhase phase) -> CLEAR_COLOR, null, 10, true, 0, null, null, 0, null);//TODO item
+		
 		// Reactions
 		// Sulfur combustion.
 		BASE_REACTIONS.add((IReactionChamber chamb) -> {
@@ -101,6 +105,14 @@ public final class AlchemyCraftingManager{
 		BASE_REACTIONS.add(new SimpleReaction(70D, 100D, -300D, null, true, Triple.of(REAGENTS[4], 1D, MatterPhase.SOLUTE), null, null, Pair.of(REAGENTS[5], 2D), null, null));
 		//Aqua Regia production
 		BASE_REACTIONS.add(new SimpleReaction(0.015625D, -300D, -300D, null, false, Triple.of(REAGENTS[5], 1D, MatterPhase.SOLUTE), Triple.of(REAGENTS[15], 3D, MatterPhase.SOLUTE), null, Pair.of(REAGENTS[11], 4D), null, null));
+		//Chlorine gas production
+		BASE_REACTIONS.add(new SimpleReaction(0D, 100D, 600D, null, true, Triple.of(REAGENTS[6], 2D, MatterPhase.SOLUTE), Triple.of(REAGENTS[4], 2D, MatterPhase.LIQUID), null, Pair.of(REAGENTS[21], 1D), Pair.of(REAGENTS[16], 2D), null));	
+		//Adamant production
+		BASE_REACTIONS.add(new SimpleReaction(0D, 100D, 600D, null, false, Triple.of(REAGENTS[20], 5D, MatterPhase.SOLUTE), null, null, Pair.of(REAGENTS[2], 1D), Pair.of(REAGENTS[6], 4D), null));
+		//Philosopher's Stone creation
+		BASE_REACTIONS.add(new SimpleReaction(-35D, -20D, -1000D, null, false, Triple.of(REAGENTS[2], 50D, MatterPhase.SOLID), Triple.of(REAGENTS[13], 50D, MatterPhase.LIQUID), Triple.of(REAGENTS[11], 1D, MatterPhase.LIQUID), Pair.of(REAGENTS[18], 100D), null, null));
+		//Alchemical Crystal production
+		BASE_REACTIONS.add(new SimpleReaction(-35D, -20D, -50D, null, false, Triple.of(REAGENTS[2], 1D, MatterPhase.SOLID), Triple.of(REAGENTS[13], 4D, MatterPhase.LIQUID), null, Pair.of(REAGENTS[22], 5D), null, null));
 		
 		// TODO reactions
 	}
