42948f011c4351163398bebdf7483eb0602a17a2
Da-Technomancer <mizrahi.max@comcast.net>
2017-09-14 21:09:18 -0400

    Fixed invalid rotary drill TE

--- src/main/java/com/Da_Technomancer/crossroads/tileentities/rotary/RotaryDrillTileEntity.java
+++ src/main/java/com/Da_Technomancer/crossroads/tileentities/rotary/RotaryDrillTileEntity.java
@@ -4,6 +4,7 @@ import com.Da_Technomancer.crossroads.API.Capabilities;
 import com.Da_Technomancer.crossroads.API.Properties;
 import com.Da_Technomancer.crossroads.API.rotary.IAxisHandler;
 import com.Da_Technomancer.crossroads.API.rotary.IAxleHandler;
+import com.Da_Technomancer.crossroads.blocks.ModBlocks;
 
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.nbt.NBTTagCompound;
@@ -32,6 +33,10 @@ public class RotaryDrillTileEntity extends TileEntity implements ITickable{
 
 	@Override
 	public void update(){
+		if(world.getBlockState(pos).getBlock() != ModBlocks.rotaryDrill){
+			invalidate();
+			return;
+		}
 		EnumFacing facing = world.getBlockState(pos).getValue(Properties.FACING);
 		if(world.isRemote){
 			TileEntity attachedTE = world.getTileEntity(pos.offset(facing.getOpposite()));
