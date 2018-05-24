ec90ce653252ad49eb56e473f0f697b8edeefd76
Da-Technomancer <mizrahi.max@comcast.net>
2018-05-24 15:57:05 -0400

    Rift now respects the spawn event
    *Rift beams now respect the onLivingSpawn event. This means, among other things, that rift things are now affected by salt burning braziers and magnum torches.

--- src/main/java/com/Da_Technomancer/crossroads/API/effects/RiftEffect.java
+++ src/main/java/com/Da_Technomancer/crossroads/API/effects/RiftEffect.java
@@ -1,7 +1,5 @@
 package com.Da_Technomancer.crossroads.API.effects;
 
-import java.util.Random;
-
 import net.minecraft.block.BlockSilverfish;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EnumCreatureType;
@@ -12,11 +10,15 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldServer;
 import net.minecraft.world.biome.Biome;
+import net.minecraftforge.event.ForgeEventFactory;
+import net.minecraftforge.fml.common.eventhandler.Event;
+
+import java.util.Random;
 
 public class RiftEffect implements IEffect{
 
 	private static final Random rand = new Random();
-	
+
 	@Override
 	public void doEffect(World worldIn, BlockPos pos, double mult){
 		if(worldIn.getBlockState(pos).getBlock() == Blocks.PURPUR_BLOCK){
@@ -27,32 +29,34 @@ public class RiftEffect implements IEffect{
 			worldIn.spawnEntity(shulker);
 			return;
 		}
-		
+
 		if(worldIn.getTileEntity(pos) instanceof TileEntitySkull && ((TileEntitySkull) worldIn.getTileEntity(pos)).getSkullType() == 0){
 			int meta = worldIn.getBlockState(pos).getBlock().getMetaFromState(worldIn.getBlockState(pos));
-			worldIn.setBlockState(pos, Blocks.AIR.getDefaultState(), 3);
+			//worldIn.setBlockState(pos, Blocks.AIR.getDefaultState(), 3);
 			worldIn.setBlockState(pos, Blocks.SKULL.getStateFromMeta(meta), 3);
 			((TileEntitySkull) worldIn.getTileEntity(pos)).setType(1);
 			return;
 		}
-		
+
 		if(BlockSilverfish.canContainSilverfish(worldIn.getBlockState(pos))){
 			worldIn.setBlockState(pos, Blocks.MONSTER_EGG.getDefaultState().withProperty(BlockSilverfish.VARIANT, BlockSilverfish.EnumType.forModelBlock(worldIn.getBlockState(pos))), 3);
-            return;
+			return;
 		}
-		
+
 		WorldServer worldServ = (WorldServer) worldIn;
 		if(worldServ.countEntities(EnumCreatureType.MONSTER, true) <= worldServ.playerEntities.size() * 3 * EnumCreatureType.MONSTER.getMaxNumberOfCreature() && rand.nextInt(64) < mult){
 			Biome.SpawnListEntry spawn = worldServ.getSpawnListEntryForTypeAt(EnumCreatureType.MONSTER, pos);
 			if(spawn != null){
-				EntityLiving ent = null;
 				try{
-					ent = spawn.entityClass.getConstructor(new Class[] {World.class}).newInstance(new Object[] {worldServ});
+					EntityLiving ent = spawn.entityClass.getConstructor(new Class[] {World.class}).newInstance(worldServ);
+					ent.setPosition(pos.getX(), pos.getY(), pos.getZ());
+					Event.Result r = ForgeEventFactory.canEntitySpawn(ent, worldServ, pos.getX(), pos.getY(), pos.getZ(), null);
+					if(r == Event.Result.ALLOW || r == Event.Result.DEFAULT){
+						worldServ.spawnEntity(ent);
+					}
 				}catch(Exception e){
 					e.printStackTrace();
 				}
-				ent.setPosition(pos.getX(), pos.getY(), pos.getZ());
-				worldServ.spawnEntity(ent);
 			}
 		}
 	}
@@ -65,14 +69,16 @@ public class RiftEffect implements IEffect{
 			if(worldServ.countEntities(EnumCreatureType.CREATURE, true) <= worldServ.playerEntities.size() * 10 * EnumCreatureType.CREATURE.getMaxNumberOfCreature() && rand.nextInt(64) < mult){
 				Biome.SpawnListEntry spawn = worldServ.getSpawnListEntryForTypeAt(EnumCreatureType.CREATURE, pos);
 				if(spawn != null){
-					EntityLiving ent = null;
 					try{
-						ent = spawn.entityClass.getConstructor(new Class[] {World.class}).newInstance(new Object[] {worldServ});
+						EntityLiving ent = spawn.entityClass.getConstructor(new Class[] {World.class}).newInstance(worldServ);
+						ent.setPosition(pos.getX(), pos.getY(), pos.getZ());
+						Event.Result r = ForgeEventFactory.canEntitySpawn(ent, worldServ, pos.getX(), pos.getY(), pos.getZ(), null);
+						if(r == Event.Result.ALLOW || r == Event.Result.DEFAULT){
+							worldServ.spawnEntity(ent);
+						}
 					}catch(Exception e){
 						e.printStackTrace();
 					}
-					ent.setPosition(pos.getX(), pos.getY(), pos.getZ());
-					worldServ.spawnEntity(ent);
 				}
 			}
 		}
