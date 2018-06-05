8a2b50fcafe0b2b8fc85156da0c1c059a7c4037f
Da-Technomancer <mizrahi.max@comcast.net>
2018-06-05 12:33:48 -0400

    Began documentation
    +Did the documentation in the Mysterious Journal. Have not documented in the Technician's Manual yet.
    +The Clockwork Stabilizer now has a maximum capacity to prevent integer overflows

--- src/main/java/com/Da_Technomancer/crossroads/tileentities/technomancy/ClockworkStabilizerTileEntity.java
+++ src/main/java/com/Da_Technomancer/crossroads/tileentities/technomancy/ClockworkStabilizerTileEntity.java
@@ -18,6 +18,7 @@ public class ClockworkStabilizerTileEntity extends BeamRenderTE{
 	}
 
 	private static final double RATE = 0.25D;
+	private static final int LIMIT = 0x0FFFFFFF;//While the documentation claims there is no storage limit to this device, there is of course the practical limit of 32 bits. In order to prevent an integer overflow error, a limit smaller than Integer.MAX_VALUE (but still extremely large) is imposed.
 	private MagicUnitStorage storage = new MagicUnitStorage();
 
 	public ClockworkStabilizerTileEntity(){
@@ -40,6 +41,14 @@ public class ClockworkStabilizerTileEntity extends BeamRenderTE{
 	@Override
 	protected void doEmit(MagicUnit toEmit){
 		storage.addMagic(toEmit);
+
+		//Enforce LIMIT
+		if(storage.getOutput() != null && storage.getOutput().getPower() > LIMIT){
+			MagicUnit stored = storage.getOutput();
+			storage.clear();
+			storage.addMagic(stored.mult((double) LIMIT / (double) stored.getPower(), false));
+		}
+
 		EnumFacing dir = world.getBlockState(pos).getValue(Properties.FACING);
 
 		if(!storage.isEmpty()){
