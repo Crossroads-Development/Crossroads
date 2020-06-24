1721921e93ac9a71789c84e4444d4679fda472ba
Da-Technomancer <Da-Technomancer@users.noreply.github.com>
2020-06-24 17:56:54 -0400

    Ported flame cloud renderer
    Ported the flame cloud entity renderer- practice for the much longer flying machine entity renderer.

--- src/main/java/com/Da_Technomancer/crossroads/entity/RenderFlameCoreEntity.java
+++ src/main/java/com/Da_Technomancer/crossroads/entity/RenderFlameCoreEntity.java
@@ -3,23 +3,19 @@ package com.Da_Technomancer.crossroads.entity;
 import com.Da_Technomancer.crossroads.Crossroads;
 import com.Da_Technomancer.crossroads.render.CRRenderUtil;
 import com.mojang.blaze3d.matrix.MatrixStack;
-import com.mojang.blaze3d.platform.GlStateManager;
-import com.mojang.blaze3d.systems.RenderSystem;
-import net.minecraft.client.renderer.BufferBuilder;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
-import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.culling.ClippingHelperImpl;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
-import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.util.ResourceLocation;
-import org.lwjgl.opengl.GL11;
 
 import java.awt.*;
 
 public class RenderFlameCoreEntity extends EntityRenderer<EntityFlameCore>{
 
-	private static final ResourceLocation TEXTURE = new ResourceLocation(Crossroads.MODID, "textures/particle/flame.png");
+	private static final ResourceLocation TEXTURE = new ResourceLocation(Crossroads.MODID, "textures/entities/flame.png");
 
 	protected RenderFlameCoreEntity(EntityRendererManager renderManager){
 		super(renderManager);
@@ -32,52 +28,42 @@ public class RenderFlameCoreEntity extends EntityRenderer<EntityFlameCore>{
 
 	@Override
 	public void render(EntityFlameCore entity, float entityYaw, float partialTicks, MatrixStack matrix, IRenderTypeBuffer buffer, int packedLight){
-		Color col = new Color(entity.getDataManager().get(EntityFlameCore.COLOR), true);
 
-		matrix.push();
+		Color color = new Color(entity.getDataManager().get(EntityFlameCore.COLOR), true);
+		int[] col = new int[] {color.getRed(), color.getGreen(), color.getBlue(), color.getAlpha()};
+		float scale = EntityFlameCore.FLAME_VEL * (float) entity.getDataManager().get(EntityFlameCore.TIME_EXISTED);
 
-		GlStateManager.enableBlend();
-		CRRenderUtil.setBrightLighting();
+		IVertexBuilder builder = buffer.getBuffer(RenderType.getEntityTranslucent(getEntityTexture(entity)));
 
-		GlStateManager.color4f((float) col.getRed() / 255F, (float) col.getGreen() / 255F, (float) col.getBlue() / 255F, (float) col.getAlpha() / 255F);
-		GlStateManager.translated(x, y, z);
+		CRRenderUtil.addVertexBlock(builder, matrix, -scale, -scale, -scale, 0, 0, 0, 0, -1, CRRenderUtil.BRIGHT_LIGHT, col);
+		CRRenderUtil.addVertexBlock(builder, matrix, scale, -scale, -scale, 1, 0, 0, 0, -1, CRRenderUtil.BRIGHT_LIGHT, col);
+		CRRenderUtil.addVertexBlock(builder, matrix, scale, scale, -scale, 1, 1, 0, 0, -1, CRRenderUtil.BRIGHT_LIGHT, col);
+		CRRenderUtil.addVertexBlock(builder, matrix, -scale, scale, -scale, 0, 1, 0, 0, -1, CRRenderUtil.BRIGHT_LIGHT, col);
 
-		double scale = EntityFlameCore.FLAME_VEL * (float) entity.getDataManager().get(EntityFlameCore.TIME_EXISTED);
-		GlStateManager.scaled(scale, scale, scale);
+		CRRenderUtil.addVertexBlock(builder, matrix, -scale, -scale, -scale, 0, 0, 0, -1, 0, CRRenderUtil.BRIGHT_LIGHT, col);
+		CRRenderUtil.addVertexBlock(builder, matrix, scale, -scale, -scale, 1, 0, 0, -1, 0, CRRenderUtil.BRIGHT_LIGHT, col);
+		CRRenderUtil.addVertexBlock(builder, matrix, scale, -scale, scale, 1, 1, 0, -1, 0, CRRenderUtil.BRIGHT_LIGHT, col);
+		CRRenderUtil.addVertexBlock(builder, matrix, -scale, -scale, scale, 0, 1, 0, -1, 0, CRRenderUtil.BRIGHT_LIGHT, col);
 
-		bindEntityTexture(entity);
+		CRRenderUtil.addVertexBlock(builder, matrix, -scale, -scale, -scale, 0, 0, -1, 0, 0, CRRenderUtil.BRIGHT_LIGHT, col);
+		CRRenderUtil.addVertexBlock(builder, matrix, -scale, -scale, scale, 1, 0, -1, 0, 0, CRRenderUtil.BRIGHT_LIGHT, col);
+		CRRenderUtil.addVertexBlock(builder, matrix, -scale, scale, scale, 1, 1, -1, 0, 0, CRRenderUtil.BRIGHT_LIGHT, col);
+		CRRenderUtil.addVertexBlock(builder, matrix, -scale, scale, -scale, 0, 1, -1, 0, 0, CRRenderUtil.BRIGHT_LIGHT, col);
 
-		buf.pos(-1, -1, -1).tex(0, 0).endVertex();
-		buf.pos(1, -1, -1).tex(1, 0).endVertex();
-		buf.pos(1, 1, -1).tex(1, 1).endVertex();
-		buf.pos(-1, 1, -1).tex(0, 1).endVertex();
+		CRRenderUtil.addVertexBlock(builder, matrix, -scale, -scale, scale, 0, 0, 0, 0, 1, CRRenderUtil.BRIGHT_LIGHT, col);
+		CRRenderUtil.addVertexBlock(builder, matrix, scale, -scale, scale, 1, 0, 0, 0, 1, CRRenderUtil.BRIGHT_LIGHT, col);
+		CRRenderUtil.addVertexBlock(builder, matrix, scale, scale, scale, 1, 1, 0, 0, 1, CRRenderUtil.BRIGHT_LIGHT, col);
+		CRRenderUtil.addVertexBlock(builder, matrix, -scale, scale, scale, 0, 1, 0, 0, 1, CRRenderUtil.BRIGHT_LIGHT, col);
 
-		buf.pos(-1, -1, -1).tex(0, 0).endVertex();
-		buf.pos(1, -1, -1).tex(1, 0).endVertex();
-		buf.pos(1, -1, 1).tex(1, 1).endVertex();
-		buf.pos(-1, -1, 1).tex(0, 1).endVertex();
+		CRRenderUtil.addVertexBlock(builder, matrix, -scale, scale, -scale, 0, 0, 0, 1, 0, CRRenderUtil.BRIGHT_LIGHT, col);
+		CRRenderUtil.addVertexBlock(builder, matrix, scale, scale, -scale, 1, 0, 0, 1, 0, CRRenderUtil.BRIGHT_LIGHT, col);
+		CRRenderUtil.addVertexBlock(builder, matrix, scale, scale, scale, 1, 1, 0, 1, 0, CRRenderUtil.BRIGHT_LIGHT, col);
+		CRRenderUtil.addVertexBlock(builder, matrix, -scale, scale, scale, 0, 1, 0, 1, 0, CRRenderUtil.BRIGHT_LIGHT, col);
 
-		buf.pos(-1, -1, -1).tex(0, 0).endVertex();
-		buf.pos(-1, -1, 1).tex(1, 0).endVertex();
-		buf.pos(-1, 1, 1).tex(1, 1).endVertex();
-		buf.pos(-1, 1, -1).tex(0, 1).endVertex();
-
-		buf.pos(-1, -1, 1).tex(0, 0).endVertex();
-		buf.pos(1, -1, 1).tex(1, 0).endVertex();
-		buf.pos(1, 1, 1).tex(1, 1).endVertex();
-		buf.pos(-1, 1, 1).tex(0, 1).endVertex();
-
-		buf.pos(-1, 1, -1).tex(0, 0).endVertex();
-		buf.pos(1, 1, -1).tex(1, 0).endVertex();
-		buf.pos(1, 1, 1).tex(1, 1).endVertex();
-		buf.pos(-1, 1, 1).tex(0, 1).endVertex();
-
-		buf.pos(1, -1, -1).tex(0, 0).endVertex();
-		buf.pos(1, -1, 1).tex(1, 0).endVertex();
-		buf.pos(1, 1, 1).tex(1, 1).endVertex();
-		buf.pos(1, 1, -1).tex(0, 1).endVertex();
-
-		matrix.pop();
+		CRRenderUtil.addVertexBlock(builder, matrix, scale, -scale, -scale, 0, 0, 1, 0, 0, CRRenderUtil.BRIGHT_LIGHT, col);
+		CRRenderUtil.addVertexBlock(builder, matrix, scale, -scale, scale, 1, 0, 1, 0, 0, CRRenderUtil.BRIGHT_LIGHT, col);
+		CRRenderUtil.addVertexBlock(builder, matrix, scale, scale, scale, 1, 1, 1, 0, 0, CRRenderUtil.BRIGHT_LIGHT, col);
+		CRRenderUtil.addVertexBlock(builder, matrix, scale, scale, -scale, 0, 1, 1, 0, 0, CRRenderUtil.BRIGHT_LIGHT, col);
 
 		super.render(entity, entityYaw, partialTicks, matrix, buffer, packedLight);
 	}
