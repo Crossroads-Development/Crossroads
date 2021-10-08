b02f4e4533009682a7c077c39cf2b6e98eb00714
Da-Technomancer <Da-Technomancer@users.noreply.github.com>
2021-10-08 11:46:08 -0400

    Added remaining textures
    +Added textures and models for Formulation Vat, Brewing Vat
    *Generated loottables and advancements for remaining blocks and recipes

--- src/main/java/com/Da_Technomancer/crossroads/blocks/witchcraft/BrewingVat.java
+++ src/main/java/com/Da_Technomancer/crossroads/blocks/witchcraft/BrewingVat.java
@@ -20,6 +20,10 @@ import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.shapes.IBooleanFunction;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.IBlockReader;
@@ -33,12 +37,19 @@ import java.util.List;
 
 public class BrewingVat extends ContainerBlock implements IReadable{
 
+	private static final VoxelShape SHAPE = VoxelShapes.join(VoxelShapes.block(), VoxelShapes.or(box(0.0D, 0.0D, 4.0D, 16.0D, 3.0D, 12.0D), box(4.0D, 0.0D, 0.0D, 12.0D, 3.0D, 16.0D), box(2.0D, 0.0D, 2.0D, 14.0D, 3.0D, 14.0D)), IBooleanFunction.ONLY_FIRST);
+
 	public BrewingVat(){
 		super(CRBlocks.getMetalProperty());
 		String name = "brewing_vat";
 		setRegistryName(name);
 		CRBlocks.toRegister.add(this);
 		CRBlocks.blockAddQue(this);
+	}
+
+	@Override
+	public VoxelShape getShape(BlockState state, IBlockReader world, BlockPos pos, ISelectionContext context){
+		return SHAPE;
 	}
 
 	@Override
