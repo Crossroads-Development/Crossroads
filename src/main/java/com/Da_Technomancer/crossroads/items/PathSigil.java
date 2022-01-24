7849be4da0bd0a5075db73f16b5e4c8d0288504d
Da-Technomancer <Da-Technomancer@users.noreply.github.com>
2022-01-24 11:03:25 -0500

    Fixed path sigils needing to be clicked on a block
    Possibly related to #176

--- src/main/java/com/Da_Technomancer/crossroads/items/PathSigil.java
+++ src/main/java/com/Da_Technomancer/crossroads/items/PathSigil.java
@@ -6,11 +6,12 @@ import com.Da_Technomancer.crossroads.API.MiscUtil;
 import com.Da_Technomancer.crossroads.CRConfig;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TranslatableComponent;
-import net.minecraft.world.InteractionResult;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 
 import javax.annotation.Nullable;
@@ -41,51 +42,53 @@ public class PathSigil extends Item{
 	}
 
 	@Override
-	public InteractionResult useOn(UseOnContext context){
-		if(context.getPlayer() == null){
-			return super.useOn(context);
+	public InteractionResultHolder<ItemStack> use(Level worldIn, Player playerIn, InteractionHand hand){
+		if(playerIn == null){
+			return super.use(worldIn, playerIn, hand);
 		}
-		if(context.getLevel().isClientSide){
+		if(worldIn.isClientSide){
 			AdvancementTracker.listen();
 		}
 
-		if(context.getPlayer().isShiftKeyDown()){
+		ItemStack held = playerIn.getItemInHand(hand);
+
+		if(playerIn.isShiftKeyDown()){
 			if(CRConfig.forgetPaths.get()){
-				if(path.isUnlocked(context.getPlayer())){
-					if(context.getLevel().isClientSide()){
-						MiscUtil.chatMessage(context.getPlayer(), new TranslatableComponent("tt.crossroads.path_sigil.forget", path.getLocalName()));
+				if(path.isUnlocked(playerIn)){
+					if(worldIn.isClientSide()){
+						MiscUtil.chatMessage(playerIn, new TranslatableComponent("tt.crossroads.path_sigil.forget", path.getLocalName()));
 					}
-					path.setUnlocked(context.getPlayer(), false);
-					context.getItemInHand().shrink(1);
-					return InteractionResult.CONSUME;
+					path.setUnlocked(playerIn, false);
+					held.shrink(1);
+					return InteractionResultHolder.success(held);
 				}else{
-					if(context.getLevel().isClientSide()){
-						MiscUtil.chatMessage(context.getPlayer(), new TranslatableComponent("tt.crossroads.path_sigil.forget.fail"));
+					if(worldIn.isClientSide()){
+						MiscUtil.chatMessage(playerIn, new TranslatableComponent("tt.crossroads.path_sigil.forget.fail"));
 					}
-					return InteractionResult.FAIL;
+					return InteractionResultHolder.fail(held);
 				}
 			}else{
-				if(context.getLevel().isClientSide()){
-					MiscUtil.chatMessage(context.getPlayer(), new TranslatableComponent("tt.crossroads.path_sigil.forget.fail.config"));
+				if(worldIn.isClientSide()){
+					MiscUtil.chatMessage(playerIn, new TranslatableComponent("tt.crossroads.path_sigil.forget.fail.config"));
 				}
-				return InteractionResult.FAIL;
+				return InteractionResultHolder.fail(held);
 			}
-		}else if(EnumPath.canUnlockNewPath(context.getPlayer())){
-			if(path.isUnlocked(context.getPlayer())){
-				if(context.getLevel().isClientSide()){
-					MiscUtil.chatMessage(context.getPlayer(), new TranslatableComponent("tt.crossroads.path_sigil.taken"));
+		}else if(EnumPath.canUnlockNewPath(playerIn)){
+			if(path.isUnlocked(playerIn)){
+				if(worldIn.isClientSide()){
+					MiscUtil.chatMessage(playerIn, new TranslatableComponent("tt.crossroads.path_sigil.taken"));
 				}
-				return InteractionResult.FAIL;
+				return InteractionResultHolder.fail(held);
 			}else{
-				path.setUnlocked(context.getPlayer(), true);
-				context.getItemInHand().shrink(1);
-				return InteractionResult.CONSUME;
+				path.setUnlocked(playerIn, true);
+				held.shrink(1);
+				return InteractionResultHolder.success(held);
 			}
 		}else{
-			if(context.getLevel().isClientSide()){
-				MiscUtil.chatMessage(context.getPlayer(), new TranslatableComponent("tt.crossroads.path_sigil.fail"));
+			if(worldIn.isClientSide()){
+				MiscUtil.chatMessage(playerIn, new TranslatableComponent("tt.crossroads.path_sigil.fail"));
 			}
-			return InteractionResult.FAIL;
+			return InteractionResultHolder.fail(held);
 		}
 	}
 }
