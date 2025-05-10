d22cd0eae8536c6bab1d9a411d10298bfcfe4423
Max M <Da-Technomancer@users.noreply.github.com>
2025-05-10 08:52:38 -0700

    Update CopshowiumCategory.java

--- src/main/java/com/Da_Technomancer/crossroads/integration/jei/CopshowiumCategory.java
+++ src/main/java/com/Da_Technomancer/crossroads/integration/jei/CopshowiumCategory.java
@@ -67,7 +67,7 @@ public class CopshowiumCategory implements IRecipeCategory<CopshowiumRec>{
 
 	@Override
 	public void setRecipe(IRecipeLayoutBuilder builder, CopshowiumRec recipe, IFocusGroup focuses){
-		int displaySize = 1000;
+		int displaySize = 500;
 		builder.addSlot(RecipeIngredientRole.INPUT, 51, 31).addIngredients(ForgeTypes.FLUID_STACK, recipe.getInput().getMatchedFluidStacks(displaySize)).setFluidRenderer(4000, true, 16, 64).setOverlay(fluidOverlay, 0, 0);
 		builder.addSlot(RecipeIngredientRole.OUTPUT, 111, 31).addIngredient(ForgeTypes.FLUID_STACK, new FluidStack(CRFluids.moltenCopshowium.getStill(), (int) (displaySize * recipe.getMult()))).setFluidRenderer(4000, true, 16, 64).setOverlay(fluidOverlay, 0, 0);
 	}
